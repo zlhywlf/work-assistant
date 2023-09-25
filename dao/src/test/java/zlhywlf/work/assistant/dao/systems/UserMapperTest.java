c461bbcc18d08f458c29b3527e1c7c2ef18be1a1
zlhywlf <tommietanghao@zlhywlf.onmicrosoft.com>
2023-09-25 17:57:03 +0800

    feat: 简化测试

--- dao/src/test/java/zlhywlf/work/assistant/dao/systems/UserMapperTest.java
+++ dao/src/test/java/zlhywlf/work/assistant/dao/systems/UserMapperTest.java
@@ -1,57 +1,34 @@
 package zlhywlf.work.assistant.dao.systems;
 
-import org.apache.ibatis.datasource.pooled.PooledDataSource;
-import org.apache.ibatis.mapping.Environment;
-import org.apache.ibatis.session.Configuration;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import zlhywlf.work.assistant.models.systems.User;
 
-import javax.sql.DataSource;
 import java.util.List;
 
 /**
  * @author zlhywlf
  */
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class UserMapperTest {
-
-    SqlSessionFactory sqlSessionFactory;
-
-    @BeforeAll
-    void beforeAll() {
-        DataSource dataSource = new PooledDataSource("com.mysql.cj.jdbc.Driver", "jdbc:mysql://192.168.50.54:3306/work_assistant", "assistant", "assistant");
-        Configuration configuration = new Configuration();
-        configuration.getTypeAliasRegistry().registerAliases("zlhywlf.work.assistant.models");
-        configuration.setEnvironment(new Environment("development", new JdbcTransactionFactory(), dataSource));
-        configuration.addMappers("zlhywlf.work.assistant.dao");
-        sqlSessionFactory = new SqlSessionFactoryBuilder().build(configuration);
-    }
+public class UserMapperTest extends BaseTest {
 
     @Test
     void selectAllTest() {
-        SqlSession sqlSession = sqlSessionFactory.openSession();
-        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
-        List<User> users = mapper.selectAll();
-        Assertions.assertNotNull(users);
-        Assertions.assertFalse(users.isEmpty());
-        sqlSession.close();
+        run(sqlSession -> {
+            UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+            List<User> users = mapper.selectAll();
+            Assertions.assertNotNull(users);
+            Assertions.assertFalse(users.isEmpty());
+        });
     }
 
     @Test
     void selectByNameTest() {
-        SqlSession sqlSession = sqlSessionFactory.openSession();
-        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
-        String name = "foo";
-        User user = mapper.selectByName(name);
-        Assertions.assertEquals(name, user.getName());
-        sqlSession.close();
+        run(sqlSession -> {
+            UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+            String name = "foo";
+            User user = mapper.selectByName(name);
+            Assertions.assertEquals(name, user.getName());
+        });
     }
 
 }
